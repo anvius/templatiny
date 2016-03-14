0378adbb98cb4ab5ec90843cab3a64dcca9d9551
Antonio Villamarin <ant@onio.org>
2016-03-14 10:49:17 +0100

    PHP CS Fixer

--- src/templatiny.php
+++ src/templatiny.php
@@ -1,24 +1,30 @@
 <?php
 
-  class Templatiny {
-    protected $template;
-    protected $variables = array();
-    public function __get($key) {
-    	return($this->variables[$key]);
-    }
-    public function __set($key, $value) {
-    	$this->variables[$key] = $value;
-    }
-    public function render($template = NULL, $vars = array()) {
-        $this->variables = array_merge($this->variables, $vars);
-        if(isset($template))
-        	$this->template = (file_exists($template) ? file_get_contents($template) : $template);
-        foreach($this->variables as $key => $value) {
-        	$this->template = preg_replace('/\{{3}\s*'.$key.'\s*\}{3}/', htmlentities($value), $this->template);
-        }
-        foreach($this->variables as $key => $value) {
-        	$this->template = preg_replace('/\{{2}\s*'.$key.'\s*\}{2}/', $value, $this->template);
-        }
-        return($this->template);
-    }
+  class templatiny
+  {
+      protected $template;
+      protected $variables = array();
+      public function __get($key)
+      {
+          return $this->variables[$key];
+      }
+      public function __set($key, $value)
+      {
+          $this->variables[$key] = $value;
+      }
+      public function render($template = null, $vars = array())
+      {
+          $this->variables = array_merge($this->variables, $vars);
+          if (isset($template)) {
+              $this->template = (file_exists($template) ? file_get_contents($template) : $template);
+          }
+          foreach ($this->variables as $key => $value) {
+              $this->template = preg_replace('/\{{3}\s*'.$key.'\s*\}{3}/', htmlentities($value), $this->template);
+          }
+          foreach ($this->variables as $key => $value) {
+              $this->template = preg_replace('/\{{2}\s*'.$key.'\s*\}{2}/', $value, $this->template);
+          }
+
+          return $this->template;
+      }
   }
